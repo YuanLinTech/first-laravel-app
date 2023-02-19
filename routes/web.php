8acae481c8a687d97d21b3e119190a565dbdb42f
Yuan Lin <yu.an.lintech@gmail.com>
2023-02-19 16:40:38 +1100

    committed

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function(){
-    return view('welcome'); // Returns the view "resources/views/welcome.blade.php"
-})->middleware('group');
+Route::get('/',[App\Http\Controllers\TestController::class, 'index'])->name('home');
 
 Route::get('/noaccess', function(){
     return "You do not have the access in this computer.";
